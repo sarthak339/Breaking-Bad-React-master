d24bd6a2230ea43b6cd0d43a77277bd32a9ccf59
sarthak <sarthakofficial920@gmail.com>
2022-02-01 19:12:59 +0530

    Updates

---
+++ precache-manifest.509412b898417663db3f80cba3aa5e8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c7b6b6a60105886dfe7327f1728901",
+    "url": "/Breaking-Bad-React-master/index.html"
+  },
+  {
+    "revision": "f00476c176cce055106f",
+    "url": "/Breaking-Bad-React-master/static/css/main.c9f4b9de.chunk.css"
+  },
+  {
+    "revision": "2aa9b7228d17b0bfb9ae",
+    "url": "/Breaking-Bad-React-master/static/js/2.e69fb641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-React-master/static/js/2.e69fb641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00476c176cce055106f",
+    "url": "/Breaking-Bad-React-master/static/js/main.2c6633cb.chunk.js"
+  },
+  {
+    "revision": "57f213d52657d7e547a9",
+    "url": "/Breaking-Bad-React-master/static/js/runtime-main.e14f7aa0.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-React-master/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-React-master/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-React-master/static/media/spinner.235b9b3e.gif"
+  }
+]);
